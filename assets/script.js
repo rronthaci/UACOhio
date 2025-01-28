a9e4e431a9c192e08d6774e190dc1cf928d1d47a
Rron Thaci <rronthaci10@gmail.com>
2025-01-28 19:00:57 +0100

    nav animations

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
-document.querySelectorAll('.dropdown-toggle').forEach((dropdown) => {
+/* document.querySelectorAll('.dropdown-toggle').forEach((dropdown) => {
     const dropdownInstance = new bootstrap.Dropdown(dropdown, {
       offset: [0, 30], // Adjust the x and y offsets
     });
   });
-  
+*/
+
+
